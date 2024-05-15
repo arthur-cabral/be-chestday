fe6a9941a9ceb85fe7ee473c269e34df903ab525
Arthur Cabral de Souza Martins <arthurmartins10052002@gmail.com>
2024-05-14 23:05:55 -0300

    Initializing project

---
+++ bechestday/src/main/java/com/example/bechestday/BechestdayApplication.java
@@ -0,0 +1,13 @@
+package com.example.bechestday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BechestdayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BechestdayApplication.class, args);
+	}
+
+}
