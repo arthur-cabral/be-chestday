ca3452ed11bfef318f651278c1b19b5da2d75816
Arthur Cabral de Souza Martins <arthurmartins10052002@gmail.com>
2024-05-16 01:39:53 -0300

    feature: Creating MuscularGroup enum

---
+++ bechestday/src/main/java/com/example/bechestday/enums/MuscularGroup.java
@@ -0,0 +1,14 @@
+package com.example.bechestday.enums;
+
+public enum MuscularGroup {
+    CHEST,
+    BACK,
+    BICEPS,
+    TRICEPS,
+    SHOULDERS,
+    ABDOMINALS,
+    QUADS,
+    HAMSTRINGS,
+    GLUTES,
+    CALVES
+}
